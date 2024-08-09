6d9b3ff49a6144f975d42b0d5b961f854afb315e
LanaLo19 <130603722+LanaLo19@users.noreply.github.com>
2024-08-09 11:49:46 -0300

    Update aleatorio.js

--- aleatorio.js
+++ aleatorio.js
@@ -1,4 +1,4 @@
-const nomes = ["Fernanda", "Giuliana", "Maria Eduarda", "Marcelo", "Amanda", "Gustavo", "Gabriel"];
+const nomes = ["Fernanda", "Giulia", "Flávia", "Ana", "Amanda", "Rebeca", "Bia"];
 
 export function aleatorio (lista){
     const posicao = Math.floor(Math.random()* lista.length);
